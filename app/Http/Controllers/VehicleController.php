0d651f94da353941be056a1c73d36a5d5196d97c
Juanesnc <juanesnc16@gmail.com>
2025-04-01 10:44:59 -0500

    Cambios

--- app/Http/Controllers/VehicleController.php
+++ app/Http/Controllers/VehicleController.php
@@ -182,6 +182,7 @@ class VehicleController extends Controller
                     'vehicle_id' => $vehicle->id,
                     'emergency_element_id' => $value['emergency_element_id']['value'],
                     'quantity' => $value['quantity'],
+                    'expiration_date' => $value['expiration_date'],
                 ];
                 $this->vehicleEmergencyElementRepository->store($dataSave);
             }
