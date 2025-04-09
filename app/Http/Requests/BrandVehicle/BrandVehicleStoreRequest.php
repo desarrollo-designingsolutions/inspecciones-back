d1ac8eec0e7df1783db7283dd8a678ed4c9c0a33
Juanesnc <juanesnc16@gmail.com>
2025-04-09 14:16:58 -0500

    Cambios

--- app/Http/Requests/BrandVehicle/BrandVehicleStoreRequest.php
+++ app/Http/Requests/BrandVehicle/BrandVehicleStoreRequest.php
@@ -6,6 +6,7 @@ use App\Helpers\Constants;
 use Illuminate\Contracts\Validation\Validator;
 use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\Http\Exceptions\HttpResponseException;
+use Illuminate\Validation\Rule;
 
 class BrandVehicleStoreRequest extends FormRequest
 {
@@ -18,7 +19,15 @@ class BrandVehicleStoreRequest extends FormRequest
     {
         $rules = [
             'company_id' => 'required',
-            'name' => 'required|unique:brand_vehicles,name,'.$this->id.',id,company_id,'.$this->company_id,
+            'name' => [
+                'required',
+                Rule::unique('brand_vehicles', 'name')
+                    ->ignore($this->id) // Ignorar el ID actual si es una actualización
+                    ->where(function ($query) {
+                        $query->where('company_id', $this->company_id) // Filtrar por empresa
+                            ->whereNull('deleted_at'); // Excluir registros eliminados
+                    }),
+            ],
         ];
 
         return $rules;
--- app/Http/Requests/TypeVehicle/TypeVehicleStoreRequest.php
+++ app/Http/Requests/TypeVehicle/TypeVehicleStoreRequest.php
@@ -6,6 +6,7 @@ use App\Helpers\Constants;
 use Illuminate\Contracts\Validation\Validator;
 use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\Http\Exceptions\HttpResponseException;
+use Illuminate\Validation\Rule;
 
 class TypeVehicleStoreRequest extends FormRequest
 {
@@ -18,7 +19,15 @@ class TypeVehicleStoreRequest extends FormRequest
     {
         $rules = [
             'company_id' => 'required',
-            'name' => 'required|unique:type_vehicles,name,'.$this->id.',id,company_id,'.$this->company_id,
+            'name' => [
+                'required',
+                Rule::unique('type_vehicles', 'name')
+                    ->ignore($this->id) // Ignorar el ID actual si es una actualización
+                    ->where(function ($query) {
+                        $query->where('company_id', $this->company_id) // Filtrar por empresa
+                            ->whereNull('deleted_at'); // Excluir registros eliminados
+                    }),
+            ],
         ];
 
         return $rules;
